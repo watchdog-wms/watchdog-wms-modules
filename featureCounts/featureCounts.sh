abb177b4bcc81cf91d0c92965a0ad855936d6e11
Michael Kluge <michael.kluge@bio.ifi.lmu.de>
2019-11-21 15:02:03 +0100

    adapted bash script for featureCounts version 2.0.0

--- featureCounts/featureCounts.sh
+++ featureCounts/featureCounts.sh
@@ -94,7 +94,7 @@ verifyFileExistence "$FLAGS_input"
 verifyFileExistence "$FLAGS_annotation"
 
 # create output folder
-$(createOutputFolder "$FLAGS_output")
+CAPTURE=$(createOutputFolder "$FLAGS_output")
 
 # check, type of SAF or GTF
 ANNO_TYPE=$FLAGS_annotationType
@@ -176,7 +176,7 @@ MESSAGE=$(eval "$COMMAND" 2>&1 | sed 's|%|%%|g')
 RET=$?
 
 # check, if tool run to end.
-COUNT=$(echo $MESSAGE | grep -c "Read assignment finished.")
+COUNT=$(echo $MESSAGE | grep -cP "(Read assignment finished.)|(Summary of counting results can)")
 if [ $COUNT -ne 1 ]; then
 	echoError "FeatureCounts was terminated before it finished."
 	CODE=1
