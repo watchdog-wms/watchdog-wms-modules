250f99685092e0e770e16a5f03dc852d282b70b3
Michael Kluge <michael.kluge@bio.ifi.lmu.de>
2020-10-30 21:35:48 +0100

    sraDump update - changes are backward compatible - added fasterq-dump support - added support for renamed parameter in 2.10 (--split-3 -> split-e)

--- sraDump/sraDump.sh
+++ sraDump/sraDump.sh
@@ -21,6 +21,8 @@ DEFINE_string 'outputFolder' '' 'folder in which the files should be extracted' 
 DEFINE_string 'tmpFolder' '/usr/local/storage' 'tmp folder; default: /usr/local/storage' 't'
 DEFINE_boolean 'deleteOnSuccess' '1' '[optional] deletes the SRA file when extraction was successfull' ''
 DEFINE_string 'returnFilePath' '' 'path to the return variables file' ''
+DEFINE_string 'binaryName' 'fastq-dump' '[optional] name of the sra-toolkit binary; possible values: [fastq-dump, fasterq-dump]; default: fastq-dump' 'b'
+DEFINE_integer 'threads' '1' '[optional] number of cores to use; only possible if fasterq-dump is used as binary' 'c'
 DEFINE_boolean 'version' 'false' '[optional] prints the version' 'v'
 DEFINE_boolean 'debug' 'false' '[optional] prints out debug messages.' ''
 
@@ -30,9 +32,28 @@ eval set -- "${FLAGS_ARGV}"
 printParamValues "initial parameters" # print param values, if in debug mode
 BASE=""
 
+BINARY=$FLAGS_binaryName
+THREADS=""
+if [ "$BINARY" != 'fastq-dump' ] && [ "$BINARY" != 'fasterq-dump' ]; then
+	echoError "Parameter -b must be 'fastq-dump' or 'fasterq-dump'. (see --help for details)";
+	exit $EXIT_INVALID_ARGUMENTS
+else
+	# check, if used binary is installed
+	MESSAGE=$($LIB_SCRIPT_FOLDER/checkUsedTools.sh "$BINARY" "check_shflag_tools")
+	CODE=$?
+
+	if [ $CODE -ne 0 ]; then
+		echoError "$MESSAGE"
+		exit $EXIT_TOOLS_MISSING
+	fi
+fi
+
+# get version
+SRA_TOOLKIT_VERSION=$($BINARY --version | grep ":" | cut -f 2 -d ":" | tr -d ' ')
+
+# print version
 if [ "$FLAGS_version" -eq 0 ]; then
-	MESSAGE=$(fastq-dump --version | grep ":" | cut -f 2 -d ":" | tr -d ' ')
-	echo $MESSAGE
+	echo $SRA_TOOLKIT_VERSION
 	exit $EXIT_OK
 fi
 
@@ -92,6 +113,20 @@ if [ -z "$FLAGS_outputFolder" ]; then
 	exit $EXIT_MISSING_ARGUMENTS
 fi
 
+if [ "$FLAGS_threads" -gt 128 ] || [ "$FLAGS_threads" -lt 1 ]; then
+	echoError "Parameter -t must be between [1, 128]. (see --help for details)";
+	exit $EXIT_INVALID_ARGUMENTS
+else
+	if [ "$BINARY" == 'fasterq-dump' ]; then
+		THREADS=" --threads $FLAGS_threads "
+	else
+		if [ "$FLAGS_threads" -gt 1 ]; then
+			FLAGS_threads=1
+			echoWarn "Binary 'fastq-dump' does only support single-threaded processing."
+		fi
+	fi
+fi
+
 printParamValues "parameters before actual script starts" # print param values, if in debug mode
 ##################################################### START with actual SCRIPT ##################################################### 
 # ensure that the parent folder is there
@@ -105,11 +140,26 @@ else
 	TMP_OUT_FOLDER=$OUT_FOLDER
 fi
 
+# format numeric software versions to allow to compare them easily
+function version { printf "%03d%03d%03d" $(echo "$1" | tr '.' ' '); }
+# replace third line with '+' and emulate -F flag of fastq-dump (--> replace '@SRA_ID READ_ID length=n' with '@READ_ID')
+function cleanFastq { awk '{if(NR % 4 == 3) {print "+"} else {print $0}}' "$1" | awk '{if(NR % 4 == 1) {sub(/^@[^ ]+? /, "@", $0); sub(/\W+length=[0-9]+$/, "", $0); print $0} else {print $0}}'; }
+
 for I in "${!FILES[@]}"; do 
 	FILE="${FILES[$I]}"
 	echo "extracting $FILE..."
 
-	executeCommand "fastq-dump -F --split-spot --skip-technical --split-3 -O \"$TMP_OUT_FOLDER\" \"$FILE\"" "/dev/null" "fastq-dump"
+	SPLIT_FLAG_NAME="--split-3"
+	TMP_FOLDER_PARAM=""
+	if [ "$BINARY" == 'fastq-dump' ] && [ $(version $SRA_TOOLKIT_VERSION) -ge $(version "2.10") ]; then
+	    SPLIT_FLAG_NAME="--split-e"
+	else 
+		if [ "$BINARY" == 'fasterq-dump' ]; then
+			TMP_FOLDER_PARAM=" --temp ${TMP_OUT_FOLDER}"
+		fi
+	fi
+
+	executeCommand "$BINARY --split-spot --skip-technical $SPLIT_FLAG_NAME $THREADS $TMP_FOLDER_PARAM -O \"$TMP_OUT_FOLDER\" \"$FILE\"" "/dev/null" "sra-dump"
 	B=$(basename $FILE ".sra")
 
 	# check, the files that were written
@@ -135,18 +185,17 @@ for I in "${!FILES[@]}"; do
 	if [ ! -z "$FLAGS_rename" ]; then
 		NAME="${RENAME[$I]}"
 	fi
+	
 	# clean the data
 	if [ "$PAIRED" == "false" ]; then
-		mv "$TMP_OUT_FOLDER/${B}.fastq" "$TMP_OUT_FOLDER/${B}.fastq.tmp"
-		awk '{if(NR % 4 == 3) {print "+"} else {print $0}}' "$TMP_OUT_FOLDER/${B}.fastq.tmp" >> "$FLAGS_outputFolder/${NAME}.fastq"
-		rm "$TMP_OUT_FOLDER/${B}.fastq.tmp"
+		cleanFastq "$TMP_OUT_FOLDER/${B}.fastq" >> "$FLAGS_outputFolder/${NAME}.fastq"
+		rm "$TMP_OUT_FOLDER/${B}.fastq"
 		CREATED_FILES="$FLAGS_outputFolder/${NAME}.fastq"
 	else
-		mv "$TMP_OUT_FOLDER/${B}_1.fastq" "$TMP_OUT_FOLDER/${B}_1.fastq.tmp"
-		mv "$TMP_OUT_FOLDER/${B}_2.fastq" "$TMP_OUT_FOLDER/${B}_2.fastq.tmp"
-		awk '{if(NR % 4 == 3) {print "+"} else {if(NR % 4 == 1) {print $0"/1"} else {print $0}}}' "$TMP_OUT_FOLDER/${B}_1.fastq.tmp" >> "$FLAGS_outputFolder/${NAME}_R1.fastq"
-		awk '{if(NR % 4 == 3) {print "+"} else {if(NR % 4 == 1) {print $0"/2"} else {print $0}}}' "$TMP_OUT_FOLDER/${B}_2.fastq.tmp" >> "$FLAGS_outputFolder/${NAME}_R2.fastq"
-		rm "$TMP_OUT_FOLDER/${B}_1.fastq.tmp" "$TMP_OUT_FOLDER/${B}_2.fastq.tmp"
+		cleanFastq "$TMP_OUT_FOLDER/${B}_1.fastq" >> "$FLAGS_outputFolder/${NAME}_R1.fastq"
+		cleanFastq "$TMP_OUT_FOLDER/${B}_2.fastq" >> "$FLAGS_outputFolder/${NAME}_R2.fastq"
+		rm "$TMP_OUT_FOLDER/${B}_1.fastq" 
+		rm "$TMP_OUT_FOLDER/${B}_2.fastq"
 		CREATED_FILES="$FLAGS_outputFolder/${NAME}_R1.fastq,$FLAGS_outputFolder/${NAME}_R2.fastq"
 	fi
 
